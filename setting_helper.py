1759b8a5dac102afb37c2da39dd9d7bbfed5aa5b
altansnl <altansenel@sabanciuniv.edu>
2022-08-13 16:23:58 +0200

    Set XLA_FLAGS

--- setting_helper.py
+++ setting_helper.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 def set_settings(seed):
     os.environ['TF_GPU_THREAD_MODE'] = 'gpu_private'
     os.environ['TF_GPU_THREAD_COUNT'] = '1'
+    os.environ['XLA_FLAGS'] = '--xla_gpu_cuda_data_dir=C:/Users/altan/miniconda3/envs/tf/Library/bin'
 
     tf.keras.utils.set_random_seed(seed)
     # tf.config.experimental.enable_op_determinism()
